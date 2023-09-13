1ec71e85ebc196c2179644bf3c8df3542fcce4cb
duynd243 <duyndse@gmail.com>
2023-09-13 13:29:43 +0700

    Update revalidate time

--- src/pages/[...wordpressNode].tsx
+++ src/pages/[...wordpressNode].tsx
@@ -7,7 +7,7 @@ export default function Page(props: WordPressTemplateProps) {
 }
 
 export const getStaticProps: GetStaticProps = (ctx) => {
-  return getWordPressProps({ ctx });
+  return getWordPressProps({ ctx, revalidate: 10 });
 };
 
 export const getStaticPaths: GetStaticPaths = () => {
